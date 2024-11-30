0ef29a53723526c40c0a221627da9c9d431959b0
Mohamed Abdelazem <mabdelazemahmed@gmail.com>
2024-11-30 23:24:26 +0300

    Initial Commit

---
+++ cmd/root.go
@@ -0,0 +1,45 @@
+/*
+Copyright © 2024 MOHAMED ABDELAZEM <mabdelazemahmed@gmail.com>
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "initiator",
+	Short: "A CLI tool to scaffold and manage Go project structures",
+	Long: `Initiator is a powerful command-line tool designed to simplify Go development.
+It helps developers generate scalable Go project structures with pre-defined templates
+and integrates seamlessly with popular tools and frameworks. Whether you're starting
+a new web service, CLI app, or library, Initiator provides the essential scaffolding
+and features to get started quickly.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) {},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.initiator.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
